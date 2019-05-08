7eab46d6cf000f94f51240fb3abba46188b8268e
Balaji Veeramani <bveeramani@berkeley.edu>
2019-05-08 01:11:26 -0700

    Fix style

--- build_model_torch.py
+++ build_model_torch.py
@@ -22,9 +22,11 @@ from mailer import send_email
 
 import torch
 
+
 def main():
     parser = argparse.ArgumentParser(description='Train model')
-    parser.add_argument('--disable-cuda', action='store_true',
+    parser.add_argument('--disable-cuda',
+                        action='store_true',
                         help='Disable CUDA')
     parser.add_argument('filename', help='Train file')
     args = parser.parse_args()
@@ -35,32 +37,36 @@ def main():
     else:
         args.device = torch.device('cpu')
 
-    print("Start training process on %s @ time" % args.filename, datetime.datetime.now())
+    print("Start training process on %s @ time" % args.filename,
+          datetime.datetime.now())
 
-    send_email("[CHTC Job Update]", "Training job started on file %s @ time %s" % (args.filename, datetime.datetime.now()))
+    send_email(
+        "[CHTC Job Update]", "Training job started on file %s @ time %s" %
+        (args.filename, datetime.datetime.now()))
     try:
         train(args.filename)
-        send_email("[CHTC Job Update]", "Ending job successfully on file %s @ time %s" % (args.filename, datetime.datetime.now()))
+        send_email(
+            "[CHTC Job Update]",
+            "Ending job successfully on file %s @ time %s" %
+            (args.filename, datetime.datetime.now()))
     except Exception as e:
         stack_trace = traceback.format_exc()
         send_email(
             "[CHTC Job Update]",
-            "Ended job with error %s on file %s @ time %s.\n\n%s" % (str(e), args.filename, datetime.datetime.now(), stack_trace))
+            "Ended job with error %s on file %s @ time %s.\n\n%s" %
+            (str(e), args.filename, datetime.datetime.now(), stack_trace))
     print("End training @ time", datetime.datetime.now())
 
 
-"""
-@param  filename    filename consisting of images to be trained on
-@return data        a dict of training and testing data, where the
-                    training data is split into an array of batches
-"""
 def dataloader(filename, batch_size=16):
+    """
+    @param  filename    filename consisting of images to be trained on
+    @return data        a dict of training and testing data, where the
+                        training data is split into an array of batches
+    """
     with open(os.path.join(assets.TRAINING_PATH, filename), 'r') as f:
         lines = [line.strip('\n') for line in f]
-    data = {
-        "train": [],
-        "test": []
-    }
+    data = {"train": [], "test": []}
     x, y = [], []
     print("Preprocessing training data...")
     for file in lines:
@@ -68,10 +74,10 @@ def dataloader(filename, batch_size=16):
         if mask is not None:
             x.append(np.moveaxis(img, -1, 0))
             y.append(mask)
-    x_train = np.array(x[:int(0.75*len(lines))])
-    y_train = np.array(y[:int(0.75*len(lines))])
-    x_test = np.array(x[int(0.75*len(lines)):])
-    y_test = np.array(y[int(0.75*len(lines)):])
+    x_train = np.array(x[:int(0.75 * len(lines))])
+    y_train = np.array(y[:int(0.75 * len(lines))])
+    x_test = np.array(x[int(0.75 * len(lines)):])
+    y_test = np.array(y[int(0.75 * len(lines)):])
     num_batches = len(x_train) // batch_size
     x_train = torch.tensor(x_train).type(torch.FloatTensor)
     y_train = torch.tensor(y_train).type(torch.LongTensor)
@@ -82,19 +88,19 @@ def dataloader(filename, batch_size=16):
     for batch_id in range(num_batches):
         print("Building batch %d / %d" % (batch_id, num_batches))
         num_points = batch_size
-        data["train"].append((
-            x_train[processed:processed+num_points],
-            y_train[processed:processed+num_points]))
+        data["train"].append((x_train[processed:processed + num_points],
+                              y_train[processed:processed + num_points]))
         processed += num_points
     print("Building test data...")
     for img, mask in zip(x_test, y_test):
-        data["test"].append((
-            torch.tensor(img).type(torch.FloatTensor),
-            torch.tensor(mask).type(torch.LongTensor)))
+        data["test"].append((torch.tensor(img).type(torch.FloatTensor),
+                             torch.tensor(mask).type(torch.LongTensor)))
     print("Done loading data...")
     return data
 
+
 class CNN(nn.Module):
+
     def __init__(self, num_classes=10):
         super(CNN, self).__init__()
         self.layer1 = nn.Sequential(
@@ -144,11 +150,12 @@ class CNN(nn.Module):
         out = F.interpolate(self.layer6(out), scale_factor=2)
         return out
 
-"""
-@param  model       the model being trained
-@param  data_file  list of filenames to be training
-"""
+
 def train(data_file):
+    """
+    @param  model       the model being trained
+    @param  data_file  list of filenames to be training
+    """
     batch_size = 64
     num_epochs = 32
     data = dataloader(data_file, batch_size)
@@ -159,7 +166,7 @@ def train(data_file):
     # print("Input image shape:", data["train"][0][0].shape)
     # print("Output mask shape:", data["train"][0][1].shape)
     print("Num testing datapoints:", len(data["test"]))
-    print("="*50)
+    print("=" * 50)
     print("\n")
 
     cnn_model = CNN()
@@ -180,7 +187,8 @@ def train(data_file):
 
             losses.append(loss.item())
             # if (i+1) % 100 == 0:
-            print ('Epoch : %d/%d, Iter : %d/%d,  Loss: %.4f' % (epoch+1, num_epochs, i+1, len(images), loss.item()))
+            print('Epoch : %d/%d, Iter : %d/%d,  Loss: %.4f' %
+                  (epoch + 1, num_epochs, i + 1, len(images), loss.item()))
 
     epoch_time = int(time.time())
     model_directory = assets.DATA_PATH + "/ml/{0}".format(epoch_time)
