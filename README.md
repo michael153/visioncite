2437d430203e3dc2ff60a75a3421ecc9e1643054
Balaji Veeramani <bveeramani@berkeley.edu>
2019-06-13 17:04:56 -0500

    Update compile script

--- README.md
+++ README.md
@@ -2,6 +2,13 @@
 
 Eyecite is an in-progress tool that allows users to accurately cite webpages.
 
+### Compiling Python
+To compile Python on CHTC, run `condor_submit -i compile.sub`. After the
+interactive session has started, run `chmod +x compile.sh` and then
+`./compile.sh`. Once the script has finished running, enter `exit` to exit the
+interactive session. You should see a zipped file named `python.tar.gz` in
+your home directory after following these instructions.
+
 ### Usage
 ```
 usage: train.py [-h] [--xtest XTEST] [--ytest YTEST] [--batches BATCH_SIZE]
--- compile.sh
+++ compile.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
-PYTHON_URL=https://www.python.org/ftp/python/3.6.8/Python-3.6.8.tgz
-PYTHON_DIRECTORY=Python-3.6.8.tgz
+PYTHON_DOWNLOAD_URL=https://www.python.org/ftp/python/3.6.8/Python-3.6.8.tgz
+PYTHON_DOWNLOAD_FILENAME=Python-3.6.8.tgz
 
-curl -o $PYTHON_DIRECTORY $PYTHON_URL
+curl -o $PYTHON_DOWNLOAD_FILENAME $PYTHON_DOWNLOAD_URL
 
 mkdir python
-tar -xzf $PYTHON_DIRECTORY
+tar -xzf $PYTHON_DOWNLOAD_FILENAME
 
 LD_LIBRARY_PATH=/usr/local/lib64/:$LD_LIBRARY_PATH
 export LD_LIBRARY_PATH
 
-cd $PYTHON_DIRECTORY
+cd "${PYTHON_DOWNLOAD_FILENAME%.*}"
 ./configure --prefix=$(pwd)/../python
 make
 make install
@@ -38,4 +38,5 @@ pip install -U matplotlib
 pip install sendgrid
 
 tar -czvf python.tar.gz python/
+rm Python-3.6.8.tgz
 exit
