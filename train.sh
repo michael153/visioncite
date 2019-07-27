f2ba89ace174191821015865e95c58f11df27c19
Balaji Veeramani <bveeramani@berkeley.edu>
2019-07-27 17:07:30 -0500

    Update submit file

--- train.sh
+++ train.sh
@@ -7,13 +7,9 @@ mkdir home
 export HOME=$(pwd)/home
 
 # Train model
-cd visioncite
 python train.py
-cd ..
+ls
 
 # Mail results
 mail.py "Training Results" "$(cat train.log)" bveeramani@berkeley.edu
 mail.py "Training Results" "$(cat train.log)" m.wan@berkeley.edu
-
-# Export model
-tar -czvf output.tar.gz ./
