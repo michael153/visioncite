0e635721b7e8cbc92782c2134483a5a0c7dcfb87
Balaji Veeramani <bveeramani@berkeley.edu>
2019-08-10 13:10:34 -0500

    Update train.sh

--- train.sh
+++ train.sh
@@ -8,6 +8,8 @@ export HOME=$(pwd)/home
 
 # Train model
 python train.py &> train.tb
+# Print to Condor log
+cat train.tb
 
 # Mail results
 python mail.py --subject "Training Results" --message "$(cat train.tb)" bveeramani@berkeley.edu
